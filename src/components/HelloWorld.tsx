7406f23421b277006dc0904cb4667bfd54036503
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-01-31 15:25:04 +0900

    create editorconfig

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Styles from '@styles/hello-world.module.scss';
+import Styles from "@styles/hello-world.module.scss";
 
 export default ({ name = "World" }: { name: string }) => {
   const [count, setCount] = useState(1);
