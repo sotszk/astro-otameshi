d93f0306f9f61894239ce43af2bd881417a6068e
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-01-31 15:32:35 +0900

    環境変数のタイピング

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_PLACE: string;
+  // 環境変数が増えたら追加する
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
