ecd87518b86bed71e1afe3e374e3a2d3359c5215
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-01 17:59:12 +0900

    公式ドキュメントを読み進めながら色々試す

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Typed from 'typed.js';
 
 import Styles from '@styles/hello-world.module.scss';
 
@@ -7,15 +8,21 @@ const place = import.meta.env.PUBLIC_PLACE;
 export default ({ name = "World" }: { name: string }) => {
   const [count, setCount] = useState(1);
 
+  useEffect(() => {
+    new Typed("#hello-typed", {
+      strings: ["hello", "I use", "typed.js"],
+    });
+  }, []);
+
   return (
     <>
       <h1 className={Styles.heading}>Hello, {name}!</h1>
+      <div id="hello-typed"></div>
       <p>Public Place: {place ? place : "設定されていません"}</p>
       <div>
         <input
           type="text"
           onChange={() => {
-            console.log("?");
             setCount((count) => (count += 1));
           }}
         />
